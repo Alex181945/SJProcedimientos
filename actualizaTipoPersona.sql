f508edaec67d74d41d76b981f3d7d16b03377834
albertorbs <alberto.roblesortega@gmail.com>
2018-03-26 23:36:03 -0600

    actualizaTipoPersona
    @alex181945 @bogar951301 @jennihdez

---
+++ actualizaTipoPersona.sql
@@ -0,0 +1,160 @@
+/**
+ * 
+ * Autor: Alberto Robles
+ * Fecha: 26/03/2018
+ * Descripcion: Procedimiento que inserta Tipo Persona
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alberto Robles 26/03/2018 In-15 Fn-19 
+ * 
+ * Notas: 0 es igual a falso, 1 es igual a verdadero
+ */
+ 
+ /*Delimitador de bloque*/
+ DELIMITER //
+
+ CREATE PROCEDURE actualizaTipoPersona( IN iIDTipoPersona INTEGER(50),
+ 										IN cTipoPersona   VARCHAR(30),
+ 										IN cNombre 		  VARCHAR(150),
+ 										IN cApePaterno    VARCHAR(150,
+ 										IN cApeMaterno	  VARCHAR(150),
+ 										IN lGenero        TINYINT(1),
+ 										IN dtFechaNac	  DateTime(100),
+ 										IN lActivo        TINYINT(1),
+ 										IN dtModificado   DateTime(10),
+ 										OUT lError        TINYINT(1), 
+ 										OUT cSqlState     VARCHAR(50), 
+ 										OUT cError        VARCHAR(200)
+ 									)
+
+ 	/*Nombre del Procedimiento*/
+ 	actualizaTipoPersona:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepxcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+		
+		START TRANSACTION;
+
+			/* Procedimientos */
+
+			/*Variables para control de errores inicializadas*/
+			SET lError    = 0;
+			SET cSqlState = "";
+			SET cError    = "";
+
+			/*Valida el Tipo Tipo Persona que crea el registro*/
+			IF NOT EXISTS(SELECT * FROM ctTipoPersonona WHERE ctTipoPersonona.iIDTipoPersona = iIDTipoPersona)
+
+				THEN
+					SET lError = 1; 
+					SET cError = "El  Tipo Persona del sistema no existe";
+					LEAVE actualizaTipoPersona;
+
+			END IF;
+
+			/*Verifica que el Tipo Persona a crear no exista con anterioridad*/
+			IF EXISTS(SELECT * FROM ctTipoPersonona  WHERE ctTipoPersonona.IDTipoPersona = iIDTipoPersona AND ctTipoPersona.lActivo = 1)
+
+				THEN 
+					SET lError = 1; 
+					SET cError = "El Tipo Persona ya existe";
+					LEAVE actualizaTipoPersona;
+
+			END IF;
+
+			/*Se valida que los dato no se encunetre nulos o vacios respecto a la tabla*/
+
+			IF cTipoPersona = "" OR cTipoPersona = NULL
+
+				THEN
+						SET lEror = 1;
+						SET cError = "El Tipo Persona no tiene valor";
+						LEAVE actualizacTipoPersona;
+			END IF;
+
+
+			IF cNombre = "" OR cNombre = NULL
+
+				THEN	
+						SET lError = 1;
+						SET cError = "El Nombre no tiene valor";
+						LEAVE actualizacNombre;
+
+			END IF;
+
+			IF cApePaterno = "" cApePaterno = NULL
+
+				THEN
+						SET lError = 1;
+						SET cError = "El Apellido Paterno no tiene valor";
+						LEAVE actualizacApePaterno;
+
+			END IF;
+
+
+			IF cApeMaterno = "" cApeMaterno = NULL
+
+				THEN
+						SET lError = 1;
+						SET cError = "El Apellido Materno no tiene valor";
+						LEAVE actualizacApeMaterno;
+
+			END IF;
+
+			IF lGenero = "" lGenero = NULL
+
+				THEN
+						SET lError = 1;
+						SET cError = "El Genero no tiene valor";
+						LEAVE actualizalGenero;
+
+			END IF;
+
+			IF dtFechaNac = "" dtFechaNac = NULL
+
+				THEN
+						SET lError = 1;
+						SET cError = "La Fecha de Nacimiento no tiene valor";
+						LEAVE actualizadtFechaNac;
+
+			END IF;
+
+
+			/*Realiza la actualizacion*/
+ 			UPDATE ctTipoPersonona
+ 						SET ctTipoPersonona.cTipoPersona = cTipoPersona,
+ 							ctTipoPersonona.cNombre      = cNombre,
+ 							ctTipoPersonona.cApePaterno  = cApePaterno,
+ 							ctTipoPersonona.cApeMaterno  = cApeMaterno,
+ 							ctTipoPersonona.lGenero      = lGenero,
+ 							ctTipoPersonona.dtFechaNac   = dtFechaNac,
+ 							ctTipoPersonona.lActivo      = lActivo,
+ 							ctTipoPersonona.dtModificado = NOE(),
+ 							WHERE ctTipoPersonona.iIDTipoPersona = iIDTipoPersona;
+
+		COMMIT;
+
+	END;//
+	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
