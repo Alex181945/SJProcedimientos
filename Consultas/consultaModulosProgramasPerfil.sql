ceb11772f582131cd911ecea217a22bb1f984b6c
Alex181945 <alestra55@hotmail.com>
2018-04-02 21:07:28 -0500

    Consultas personalizadas y consultas a los modulos por perfil

---
+++ Consultas/consultaModulosProgramasPerfil.sql
@@ -0,0 +1,66 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 02/04/2018
+ * Descripcion: Consulta personalizada para cargar los modulos y programas
+ * ligados a un determiando tipo de perfil
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo:  Bogar Chavez 13/03/2018 
+ *
+ * Nota: 0 es falso, 1 es verdadero
+ * 
+ */
+ 
+ /*Para pruebas*/
+ /*USE SENADO;*/
+
+ /*Delimitador de bloque*/
+ DELIMITER //
+
+ CREATE PROCEDURE consultaModulosProgramasPerfil(	IN iTipoConsulta INTEGER(3),
+		 											IN iIDPerfil     INTEGER(11),
+													OUT lError       TINYINT(1), 
+													OUT cSqlState    VARCHAR(50), 
+													OUT cError       VARCHAR(200))
+ 	consultaModulosProgramasPerfil:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+
+		START TRANSACTION;
+
+			/*Variables para control de errores inicializadas*/
+			SET lError    = 0;
+			SET cSqlState = "";
+			SET cError    = "";
+
+			/*Consulta*/
+			CALL consultaModulosPerfil(iTipoConsulta, iIDPerfil, lError, cSqlState, cError);
+
+		COMMIT;
+
+	END;//
+	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
