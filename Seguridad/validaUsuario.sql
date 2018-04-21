7078ca6bc74b429db9d4fef3fa3107bad8182cf2
Alex181945 <alestra55@hotmail.com>
2018-04-21 07:35:29 -0500

    Valida usuario

---
+++ Seguridad/validaUsuario.sql
@@ -0,0 +1,106 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 05/03/2018
+ * Descripcion: Procedimiento que valida usuario
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19 
+ *
+ * Nota: 0 es falso, 1 es verdadero
+ * 
+ */
+ 
+ /*Para pruebas*/
+USE escuela;
+
+ /*Delimitador de bloque*/
+ DELIMITER //
+
+ CREATE PROCEDURE validaUsuario(	IN cUsuario   VARCHAR(20),
+ 									IN cPassword  VARCHAR(20),
+ 									OUT lError    TINYINT(1), 
+ 									OUT cSqlState VARCHAR(50), 
+ 									OUT cError    VARCHAR(200))
+ 	validaUsuario:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+
+		START TRANSACTION;
+
+			/*Procedimiento*/
+
+			/*Variables para control de errores inicializadas*/
+			SET lError    = 0;
+			SET cSqlState = "";
+			SET cError    = "";
+
+			/*Crea una tabla temporal con la estructura de la tabla
+			 *especificada despues del LIKE
+			 */
+			DROP TEMPORARY TABLE IF EXISTS tt_ctUsuario;
+
+			CREATE TEMPORARY TABLE tt_ctUsuario LIKE ctUsuario;
+
+			/*Comprueba si existe el usuario*/
+			IF EXISTS(SELECT * FROM ctUsuario WHERE ctUsuario.cEmail = cUsuario)
+
+				/*Si existe copia toda la informacion del usuario a la tabla temporal*/
+				THEN INSERT INTO tt_ctUsuario SELECT * FROM ctUsuario WHERE ctUsuario.cEmail = cUsuario;
+
+				/*Si no manda error de que no lo encontro*/
+				ELSE 
+					SET lError = 1; 
+					SET cError = "Usuario no existe";
+					LEAVE validaUsuario;
+
+			END IF;
+
+			/*Valida que el usuario este activo*/
+			IF NOT EXISTS(SELECT * FROM tt_ctUsuario WHERE tt_ctUsuario.lActivo = 1)
+
+				THEN 
+					SET lError = 1; 
+					SET cError = "Usuario no activo";
+					LEAVE validaUsuario;
+
+			END IF;
+
+			IF NOT EXISTS(SELECT * FROM tt_ctUsuario WHERE tt_ctUsuario.cContrasena = SHA(cPassword))
+
+				THEN 
+					SET lError = 1; 
+					SET cError = "Usuario y/o Contraseña erroneas";
+					LEAVE validaUsuario;
+
+			END IF;
+
+			SELECT * FROM tt_ctUsuario;
+
+		COMMIT;
+
+	END;//
+	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
