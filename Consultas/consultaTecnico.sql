1f23aedb60b02966197700dec7cc71f775a0dbb8
Alex181945 <alestra55@hotmail.com>
2018-05-17 12:37:24 -0500

    Consulta de los tecnicos y su carga de trabajo por dia

---
+++ Consultas/consultaTecnico.sql
@@ -0,0 +1,76 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 17/05/2018
+ * Descripcion: Consulta tecnicos disponibles por carga de trabajo
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alejandro Estrada 09/09/2017 In-15 Fn-19 
+ *
+ * Nota: 0 es falso, 1 es verdadero
+ * 
+ */
+ 
+ /*Para pruebas*/
+USE SENADO;
+DROP PROCEDURE IF EXISTS `consultaTecnico`;
+
+/*Delimitador de bloque*/
+ DELIMITER //
+
+ CREATE PROCEDURE consultaTecnico(	IN iTipoConsulta	VARCHAR(11),
+ 									OUT lError    		TINYINT(1), 
+ 									OUT cSqlState 		VARCHAR(50), 
+ 									OUT cError    		VARCHAR(200))
+ 	consultaTecnico:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+
+		START TRANSACTION;
+
+			/*Procedimiento*/
+
+			/*Variables para control de errores inicializadas*/
+			SET lError    = 0;
+			SET cSqlState = "";
+			SET cError    = "";
+
+			CASE iTipoConsulta
+
+			    WHEN "1" THEN
+
+			    	/*Carga de Trabajo*/
+			    	SELECT iIDTecnico, COUNT(iIDTicket)
+			    		FROM opTickets 
+			    		WHERE DATE(opTickets.dtFecha) = DATE(NOW())
+			    		GROUP BY iIDTecnico ASC;
+
+			END CASE;
+
+		COMMIT;
+
+	END;//
+	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
