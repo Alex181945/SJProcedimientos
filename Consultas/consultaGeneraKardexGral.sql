c5ce959b7d9637823d1af990984506ae6ac6b3bc
Alex181945 <alestra55@hotmail.com>
2018-05-29 00:54:55 -0500

    Kardex General

---
+++ Consultas/consultaGeneraKardexGral.sql
@@ -0,0 +1,79 @@
+/**
+ * 
+ * Autor: Alejandro Estrada
+ * Fecha: 18/05/2018
+ * Descripcion: Procedimiento que actualiza el registro
+ * de un tipo de persona
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Bogar Chavez 11/03/2018 In-15 Fn-19 
+ *
+ * Nota: 0 es falso, 1 es verdadero
+ * 
+ */
+
+ /*Para pruebas*/
+ USE escuelast;
+ DROP PROCEDURE IF EXISTS `consultaGeneraKardexGral`;
+
+ /*Delimitador de bloque*/
+ DELIMITER //
+
+ CREATE PROCEDURE consultaGeneraKardexGral(	IN iPersona   INTEGER(11) ,
+ 										OUT lError    TINYINT(1), 
+ 										OUT cSqlState VARCHAR(50), 
+ 										OUT cError    VARCHAR(200))
+ 	consultaGeneraKardexGral:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+
+		START TRANSACTION;
+			
+/*Procedimiento*/
+
+			/*Variables para control de errores inicializadas*/
+			SET lError    = 0;
+			SET cSqlState = "";
+			SET cError    = "";
+			
+			IF iPersona = 0 OR iPersona = NULL
+
+				THEN 
+					SET lError = 1;
+					SET cError = "El ID de la persona no tiene valor";
+					LEAVE consultaGeneraKardexGral;
+
+			END IF;
+
+			SELECT opCalificacion.*, ctMateria.cMateria
+				FROM opCalificacion 
+					INNER JOIN ctMateria ON ctMateria.iMateria = opCalificacion.iMateria
+			WHERE opCalificacion.iPersona      = iPersona;
+				
+		COMMIT;
+
+	END;//
+	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
