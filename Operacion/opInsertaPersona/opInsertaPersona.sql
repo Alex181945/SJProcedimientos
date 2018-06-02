f90af19b6f07224c0bcd21f8966d7401e2c77d68
Alex181945 <alestra55@hotmail.com>
2018-06-01 22:42:18 -0500

    ok

---
+++ Operacion/opInsertaPersona/opInsertaPersona.sql
@@ -0,0 +1,58 @@
+/**
+ * 
+ * Autor: Alberto Robles
+ * Fecha: 16/03/2018
+ * Descripcion: Procedimiento que inserta Tipo Persona
+ *  
+ * Modificaciones:
+ * <Quien modifico:> <Cuando modifico:> <Donde modifico:>
+ * Ejemplo: Alberto Robles 16/03/2018 In-15 Fn-19 
+ * 
+ * Notas: 0 es igual a falso, 1 es igual a verdadero
+ */
+ 
+ /*Delimitador de bloque*/
+
+ USE escuelast;
+
+ DELIMITER //
+
+ CREATE PROCEDURE opInsertaPersona( IN cCarrera  VARCHAR(100),
+		 							OUT lError      TINYINT(1), 
+		 							OUT cSqlState   VARCHAR(50), 
+		 							OUT cError      VARCHAR(200))
+
+ 	/*Nombre del Procedimiento*/
+ 	opInsertaPersona:BEGIN
+
+		/*Manejo de Errores*/ 
+		DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@e1 = RETURNED_SQLSTATE, @e2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @e1);
+			SET cError    = CONCAT("Exepxcion: ", @e2);
+			ROLLBACK;
+		END; 
+
+		/*Manejo de Advertencias*/ 
+		DECLARE EXIT HANDLER FOR SQLWARNING
+		BEGIN
+			GET DIAGNOSTICS CONDITION 1
+  			@w1 = RETURNED_SQLSTATE, @w2 = MESSAGE_TEXT;
+			SET lError    = 1;
+			SET cSqlState = CONCAT("SqlState: ", @w1);
+			SET cError    = CONCAT("Advertencia: ", @w2);
+			ROLLBACK;
+		END;	 
+		
+		START TRANSACTION;
+
+			/* Procedimientos */
+
+			
+		COMMIT;
+	END;//	
+ /*Reseteo del delimitador*/	
+ DELIMITER ;
